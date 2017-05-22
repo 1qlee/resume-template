0251e28b5c234fc70828680b1e77fb32a494a79e
wonkyu <wonq33@gmail.com>
2017-05-22 13:42:20 -0400

    Started adding custom styling; building generic template showcase page

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 var express = require('express');
-
 var app = express();
+var server = require('http').createServer(app);
+var port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname));
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
+
+app.use(express.static(__dirname + "/public"));
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
 app.get('/', function(req, res) {
   res.render('index');
-});
-
-app.listen(3000, function() {
-  console.log("Listening on port 3000.");
 });
